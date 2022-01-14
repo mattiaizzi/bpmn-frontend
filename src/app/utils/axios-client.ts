19d7ff672d82277243f2b902ef9fdc82f3256b61
Mattia Izzi <izzimattia@yahoo.it>
2022-01-15 00:21:39 +0100

    added docker file

--- src/app/utils/axios-client.ts
+++ src/app/utils/axios-client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:6868';
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:6868';
 
 const client = axios.create({
     baseURL,
